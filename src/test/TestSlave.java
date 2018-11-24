ca50a4f4db7f553c084f950c2769b52138f2b68d
bkotak <bhavinkotak07@gmail.com>
2018-11-24 18:10:05 +0530

    Added hash MD5 hash function

--- src/test/TestSlave.java
+++ src/test/TestSlave.java
@@ -3,21 +3,37 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import org.json.simple.JSONObject;
 public class TestSlave {
-	public static long hashTo64bit(String string) {
-        long h = 1125899906842597L;
-        int len = string.length();
+    
+    public static long hash(String key) {
+    	MessageDigest instance = null;
+		try {
+			instance = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        instance.reset();
+        instance.update(key.getBytes());
+        byte[] digest = instance.digest();
 
-        for (int i = 0; i < len; i++) {
-            h = (31 * h) + string.charAt(i);
+        long h = 0;
+        for (int i = 0; i < 4; i++) {
+            h <<= 8;
+            h |= ((int) digest[i]) & 0xFF;
         }
-        return Math.abs(h);
+        return h;
     }
+   
 	public static void main(String []args) {
 		try {
-			
+			System.out.println(hashTo64bit(Integer.toString(8081)));
+			System.out.println(hashTo64bit(Integer.toString(8082)));
+/*
 			Socket socket = new Socket("127.0.0.1", 5651);
 			DataOutputStream op = new DataOutputStream(socket.getOutputStream());
 			JSONObject obj = new JSONObject();
@@ -34,7 +50,7 @@ public class TestSlave {
 			op.close();
 			socket.close();
 			//
-			 
+			 */
 			 
 			//System.out.println(hashTo64bit("kotak"));
 			
